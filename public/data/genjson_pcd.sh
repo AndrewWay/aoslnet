27a672d73e856ef8c6fe98a66f09c382386a58ab
Andrew Way <arw405@mun.ca>
2017-05-26 16:02:48 -0230

    new script specifically for creating JSON files from SeaDragon PCD files Added "fullberg.txt"; text file that has the sonar and lidar data for 2017-05-25 iceberg. Will rename that file soon.

--- public/data/genjson_pcd.sh
+++ public/data/genjson_pcd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-name="Bob"
+name="James"
 year=1980
 long=-52.732711
 lat=47.571306
@@ -8,7 +8,7 @@ height=101
 width=143
 volume=10832
 
-input="2017_05_25_sonar_filtered.ply"
+input="fullberg.txt"
 length=`cat $input | wc -l`
 
 output="$name.json"
@@ -25,31 +25,30 @@ echo '    "height" : "'$height'",' >> $output
 echo '    "width" : "'$width'",' >> $output
 echo '    "volume" : "'$volume'",' >> $output
 
-for i in `seq 1 $length`
+for i in `seq 1 $((length-1))`
 do
-
 line=`cat $input | head -n $i | tail -n 1`
 
 newx=`echo $line | awk '{print $1}'`
 newy=`echo $line | awk '{print $2}'`
 newz=`echo $line | awk '{print $3}'`
 
-xdat=$xdat$newx
-ydat=$ydat$newy
-if [ $i -eq 2 ];then
-zdat=$zdat$newz
-else 
-zdat=$zdat,$newz
-fi
+
+xdat=$xdat$newx,
+ydat=$ydat$newy,
+zdat=$zdat$newz,
 
 done
-xdat=${xdat::-1}
-ydat=${ydat::-1}
 
+line=`cat $input | head -n $length | tail -n 1`
 
-xdat="$xdat],"
-ydat="$ydat],"
-zdat="$zdat]"
+newx=`echo $line | awk '{print $1}'`
+newy=`echo $line | awk '{print $2}'`
+newz=`echo $line | awk '{print $3}'`
+
+xdat="$xdat$newx],"
+ydat="$ydat$newy],"
+zdat="$zdat$newz]"
 echo "    $xdat" >> $output
 echo "    $ydat" >> $output
 echo "    $zdat" >> $output
