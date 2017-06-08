d2d8c513fc2d88c8d1f90289d6dc0dc7fb1e1639
Andrew Way <arw405@mun.ca>
2017-06-08 13:00:37 -0230

    Failed attempt at gallery creation

--- public/js/icebergSelector.js
+++ public/js/icebergSelector.js
@@ -206,8 +206,10 @@ function updatePic(p){
   console.log('updatePic()');
   console.log('pic: '+p);
   if(!(p === 'null')){
-    
     document.getElementById("icedisp").src="../images/2017/Joey/"+p;
+    $("#icedisp").on("error", function(){
+        $(this).attr('src', 'images/notfound.png');
+    });
   }
 }
 
