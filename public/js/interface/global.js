3063b8b3212528386653c4e2ff8ad1eecb03b3f9
Andrew Way <arw405@mun.ca>
2017-06-15 15:48:00 -0230

    ctd.js now recieves all of the salinity, temperature, and depth data from distributedata.js instead of being fed pieces at a time during runtime. Timebar a work in progress. Works correctly with the STD graphs, but for some reason causes wrong image to be displayed

--- public/js/interface/global.js
+++ public/js/interface/global.js
@@ -7,7 +7,6 @@ var IcebergModelName="";
 var jsonIcberg ={};
 var mapInitialized=false;
 tmax=0;
-time_index=0;
 disp_size=20;
 delay_factor=0.5;
 playid=0;
@@ -150,5 +149,6 @@ function distributeData(dat){
         sdlat[i]=dat[i].latitudeSD;
         sdlong[i]=dat[i].longitudeSD;      
     }
+    setplotData(sal,temp,depth,time);
     console.log('distributeData() finished');
 }
--- public/js/interface/playback.js
+++ public/js/interface/playback.js
@@ -1,11 +1,13 @@
 //Trial playback
+time_index=0;
 
 function manualsetTime(){
   var newTime=document.getElementById("timebar").value;
   console.log("Manually setting time to: "+newTime);
-  time_index=newTime;
+  setime(newTime);  
+  redraw(time_index);
 }
-function setTime(t){
+function setime(t){
   time_index=t;
 }
 function setTimeBar(t){
@@ -21,21 +23,21 @@ function play(){
     document.getElementById("pausebtn").disabled=false;
     document.getElementById("stopbtn").disabled=false;
     document.getElementById("playbtn").disabled=true;
-
+    document.getElementById("timebar").disabled=true;
     playid=setInterval(function(){
         var c = cond[time_index];
         var t = temp[time_index];
         var d = depth[time_index];
         var ti = time_index;//time[time_index];//Json objects have bad timestamp data
-        redraw(c,t,d,ti);
-        var ws=windSpd[time_index];
-        var wd=windDir[time_index];        
-        var p = pics[time_index];        
+        redraw(ti);
+        var ws=windSpd[ti];
+        var wd=windDir[ti];        
+        var p = pics[ti];        
         updateWind(ws,wd);
         updatePic(p);
-        setTimeBar(time_index);        
+        setTimeBar(ti);        
         
-        setTime(time_index+1);
+        setime(ti+1);
         if(time_index>cond.length){
           clearInterval(playid);
         }
@@ -44,6 +46,7 @@ function play(){
 
 function pause(){
   clearInterval(playid);
+  document.getElementById("timebar").disabled=false;
   document.getElementById("pausebtn").disabled=true;
   document.getElementById("playbtn").disabled=false;
 }
@@ -53,6 +56,7 @@ function stop(){
   setTime(0);
   setTimeBar(time_index);
   setupChart();
+  document.getElementById("timebar").disabled=false;
   document.getElementById("stopbtn").disabled=true;
   document.getElementById("pausebtn").disabled=true;
   document.getElementById("playbtn").disabled=false;
