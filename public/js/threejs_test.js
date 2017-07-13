b6695562c6814611ed7068b8a71018660466e12b
Andrew Way <arw405@mun.ca>
2017-07-13 12:35:18 -0230

    Modified lighting and colors of model viewer

--- public/js/threejs_test.js
+++ public/js/threejs_test.js
@@ -7,14 +7,14 @@ var camera, controls, scene, renderer;
 init();
 render();
  // remove when using next line for animation loop (requestAnimationFrame)
-//animate();
+animate();
 
 function init() {
 
   /* SCENE */
 
   scene = new THREE.Scene(); // Create the scene 
-
+  scene.background = new THREE.Color( 0x222222 );
   /* RENDERER */
 
 	renderer = new THREE.WebGLRenderer(); // Create the renderer
@@ -31,34 +31,40 @@ function init() {
 
 	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
 	camera.position.z = 500;
-  
-	controls = new THREE.OrbitControls( camera );
-  /*controls.staticMoving = true;	
-	controls.enableDamping = true;
-	controls.dampingFactor = 0.25;*/
+  console.log('Creating trackballcontrols');
+	controls = new THREE.TrackballControls( camera );
+  //controls = new THREE.OrbitControls(camera);  
+  controls.staticMoving = false;	
+	controls.enableDamping = false;
+	controls.dampingFactor = 0.01;
 	controls.enableZoom = true;
   controls.addEventListener( 'change', render ); // remove when using animation loop
 	// enable animation loop when using damping or autorotation
-  /* STL LOADER */
+  console.log('trackball created');
+  /* GEOMETRY */
 
   var loader = new THREE.STLLoader();
   loader.load( './data/r11i02.stl', function ( geometry ) {
     console.log('I am in the function?');
-    var material = new THREE.MeshPhongMaterial( { color: 0xff5533 } );
+    var material = new THREE.MeshPhongMaterial( { color: 0xffffff } );
     var mesh = new THREE.Mesh( geometry, material );
     mesh.material.side = THREE.DoubleSide;
     scene.add( mesh );
   });
 
+    console.log('STL loaded');
 	/* LIGHTS */
 
 	var light = new THREE.DirectionalLight( 0xffffff );
-	light.position.set( 1, 1, 1 );
+	light.position.set( 0, 0, 100 );
+  light.intensity=0.86;
 	scene.add( light );
 	var light = new THREE.DirectionalLight( 0x002288 );
-	light.position.set( -1, -1, -1 );
+	light.position.set( 0, 0, -1 );
 	scene.add( light );
-	var light = new THREE.AmbientLight( 0x222222 );
+	var light = new THREE.DirectionalLight( 0x99cfff);
+  light.position.set(0,0,100);
+  light.intensity=0.2;
 	scene.add( light );
 	//
 	stats = new Stats();
