0f81803a3bf9c7b4256ee2f5e130087836cd0aff
Andrew Way <arw405@mun.ca>
2017-07-13 10:24:26 -0230

    IT WORKS. Stl files are loaded via specifying file path on server. Only outside surface of model shows now. Will work on camera control and lighting next

--- public/js/threejs_test.js
+++ public/js/threejs_test.js
@@ -10,83 +10,48 @@ render();
 //animate();
 
 function init() {
+
+  /* SCENE */
+
   scene = new THREE.Scene(); // Create the scene 
- // scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 ); // Create the fog
+
+  /* RENDERER */
+
 	renderer = new THREE.WebGLRenderer(); // Create the renderer
 	//renderer.setClearColor( scene.fog.color ); //Set the clear color ?
 	renderer.setPixelRatio( window.devicePixelRatio ); // tch
 	renderer.setSize( window.innerWidth, window.innerHeight ); //tch
+
+  /* DOM CONTAINER FOR RENDERER */
+
 	var container = document.getElementById( 'container' ); //Assign the output to container
 	container.appendChild( renderer.domElement );
+
+  /* CAMERA */
+
 	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
 	camera.position.z = 500;
-	controls = new THREE.OrbitControls( camera, renderer.domElement );
-	controls.addEventListener( 'change', render ); // remove when using animation loop
-	// enable animation loop when using damping or autorotation
-	//controls.enableDamping = true;
-	//controls.dampingFactor = 0.25;
-	controls.enableZoom = true;
-	
-	// world
-  //getJSON(url);
   
-  console.log("Distributing Data");
+	controls = new THREE.OrbitControls( camera );
+  /*controls.staticMoving = true;	
+	controls.enableDamping = true;
+	controls.dampingFactor = 0.25;*/
+	controls.enableZoom = true;
+  controls.addEventListener( 'change', render ); // remove when using animation loop
+	// enable animation loop when using damping or autorotation
+  /* STL LOADER */
 
-  var geom = new THREE.Geometry();
-  url='/bergs/test';
-  var request = new XMLHttpRequest();
-  request.open("GET",url,false);
-  request.send(null);
-  console.log('database returned: '+request.responseText);
+  var loader = new THREE.STLLoader();
+  loader.load( './data/r11i02.stl', function ( geometry ) {
+    console.log('I am in the function?');
+    var material = new THREE.MeshPhongMaterial( { color: 0xff5533 } );
+    var mesh = new THREE.Mesh( geometry, material );
+    mesh.material.side = THREE.DoubleSide;
+    scene.add( mesh );
+  });
 
-    var s = request.responseText;
-  // "stl" represents a raw STL binary read from HTTP response data
-  var parseStlBinary = parsebinary(s);
-  console.log('finished!');
-  x=[];
-  y=[];
-  z=[];
+	/* LIGHTS */
 
-  //Scale our data
-  for(i=0;i<y.length;i++){
-    var xtmp=x[i];
-    var ytmp=y[i];
-    var ztmp=z[i];
-
-    x[i]=scale*xtmp;
-    y[i]=scale*ytmp;
-    z[i]=scale*ztmp;
-  }
-
-  var f=0;
-
-  for(i=0;i<y.length;i++){
-    newx=x[i];
-    newy=y[i];
-    newz=z[i];
-    console.log("ADDING: x "+newx+" y "+newy+" z "+newz);
-    var v = new THREE.Vector3(newx,newy,newz);
-    geom.vertices.push(v);
-  }
-
-  for(i=2;i<y.length;i++){
-    geom.faces.push( new THREE.Face3( i-2, i-1, i ) );
-    geom.faces.push( new THREE.Face3( i-1, i-2, i ) );
-    geom.computeFaceNormals();    
-  }
-
-  // Create the material
-  var material =  new THREE.MeshPhongMaterial( { color:0xffffff, shading: THREE.FlatShading } );
-  var mesh = new THREE.Mesh( geom, material );
-  mesh =  new THREE.Mesh( geom, new THREE.MeshNormalMaterial() );
-	mesh.position.x = 0;
-	mesh.position.y = 0;
-	mesh.position.z = 0;
-	mesh.updateMatrix();
-	mesh.matrixAutoUpdate = false;
-  scene.add( mesh );
-
-	// lights
 	var light = new THREE.DirectionalLight( 0xffffff );
 	light.position.set( 1, 1, 1 );
 	scene.add( light );
@@ -102,48 +67,6 @@ function init() {
 	window.addEventListener( 'resize', onWindowResize, false );
 }
 
-function parsebinary(stl) {
-    console.log('hello!');  
-    // create three.js geometry object, discussed later
-    var geo = new THREE.Geometry();
-    // The stl binary is read into a DataView for processing
-    var dv = new DataView(stl, 80); // 80 == unused header
-    var isLittleEndian = true;
-
-    // Read a 32 bit unsigned integer
-    var triangles = dv.getUint32(0, isLittleEndian);
-
-    var offset = 4;
-    for (var i = 0; i < triangles; i++) {
-      // Get the normal for this triangle by reading 3 32 but floats
-      var normal = new THREE.Vector3(
-        dv.getFloat32(offset, isLittleEndian),
-        dv.getFloat32(offset+4, isLittleEndian),
-        dv.getFloat32(offset+8, isLittleEndian)
-      );
-      offset += 12;
-
-      // Get all 3 vertices for this triangle, each represented
-      // by 3 32 bit floats.
-      for (var j = 0; j < 3; j++) {
-        geo.vertices.push(
-          new THREE.Vector3(
-            dv.getFloat32(offset, isLittleEndian),
-            dv.getFloat32(offset+4, isLittleEndian),
-            dv.getFloat32(offset+8, isLittleEndian)
-          )
-        );
-        offset += 12
-      }
-      // there's also a Uint16 "attribute byte count" that we
-      // don't need, it should always be zero.
-      offset += 2;
-
-      // Create a new face for from the vertices and the normal
-      geo.faces.push(new THREE.Face3(i*3, i*3+1, i*3+2, normal));
-    }
-    // continue parsing STL faces for rendering...
-};
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
