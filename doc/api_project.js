bd8221ea03470c440dcd354ed1494082ca6c56f6
Andrew Way <arw405@mun.ca>
2017-07-25 09:59:16 -0230

    Cleaned up the repo by removing certain untracked files and adding new tracked files

---
+++ doc/api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "API Documentation",
+  "version": "1.0.0",
+  "description": "API",
+  "template": {
+    "forceLanguage": "en"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-07-20T17:27:08.831Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
