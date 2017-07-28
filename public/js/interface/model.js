81507e929c4839c81ae5640f89337a10e8c3bd63
Andrew Way <arw405@mun.ca>
2017-07-28 10:06:41 -0230

    Added some if statements to check for height, width, and volume Reformated models and global with new indentation Decoupled updateMesh from model

--- public/js/interface/model.js
+++ public/js/interface/model.js
@@ -6,26 +6,38 @@
 
 currentfile='';
 modelcontainerid='model';
-
+xarr=[];
+yarr=[];
+zarr=[];
+cam_z=100;
 /*
  * Update the mesh by specifying a new 3D model file path
  * @param {String} file
  */
-function updateMesh(file){
-  console.log('updateMesh() starting');
-  setfile(file);
-  model();
-  console.log('updateMesh() ending');
+function updateMesh(){
+	console.log('updateMesh() starting');
+	model();
+	console.log('updateMesh() ending');
 }
 
 /*
- * Set the current 3D model file
+ * Set the current 3D model filepath on the public directory
  * @param {String} file
  */
 function setfile(file){
-  currentfile=file;
+	currentfile=file;
 }
 
+function setPointCloud(x,y,z){
+	console.log('setting the point cloud arrays in model.js');
+	console.log(x);
+	console.log(y);
+	console.log(z);
+
+	xarr=x;
+	yarr=y;
+	zarr=z;
+}
 /*
  * Update the display for the iceberg dimensions
  * @param {Number} h
@@ -33,76 +45,75 @@ function setfile(file){
  * @param {Number} v
  */
 function updateDim(h,w,v){
-  console.log("updateDim() running");
-  console.log("h: "+h+"w: "+w+"v: "+v);
-  var hTd = document.getElementById("icebergHeight");
-  var wTd = document.getElementById("icebergWidth");
-  var vTd = document.getElementById("icebergVolume");
-  hTd.innerHTML=h;
-  wTd.innerHTML=w;
-  vTd.innerHTML=v;
-  console.log("updateDim() done");
+	console.log("updateDim() running");
+	console.log("h: "+h+"w: "+w+"v: "+v);
+	var hTd = document.getElementById("icebergHeight");
+	var wTd = document.getElementById("icebergWidth");
+	var vTd = document.getElementById("icebergVolume");
+	hTd.innerHTML=h;
+	wTd.innerHTML=w;
+	vTd.innerHTML=v;
+	console.log("updateDim() done");
 }
 
-/*
- * Display 3D point cloud
- */
-
-function addpoints() {
- 
-    // grab data from the INPUT elements
-    var x = grab_float_data( "X" ) ;
-    var y = grab_float_data( "Y" ) ;
-    var z = grab_float_data( "Z" ) ;
- 
-    // loop through the points and add them to the scene
-    for( var i=0 ; i < z.length ; i++ ) {
- 
-        // geometry describes the shape
-        geometry = new THREE.SphereGeometry( 24, 16, 16 ) ;
- 
-        // material describes the surface of the shape
-        material = new THREE.MeshLambertMaterial( { color:0x00CCFF } ) ;
- 
-        // mesh maps the material onto the geometry to make an object  
-        mesh = new THREE.Mesh( geometry, material ) ;
- 
-        // position the mesh in space
-        mesh.position.set( x[i], y[i], z[i] ) ;
- 
-        // add the mesh to the scene
-        scene.add( mesh ) ;
-    }
-}
 
 /*
  * Display 3D model
  */
 function model(){  
-  if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+	if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
-  var stats;
-  var camera, controls, scene, renderer;
-  init();
-  loadModel();
-  //loadPointCloud();
-  render();
-  animate();
+	var stats;
+	var camera, controls, scene, renderer;
+	init();
+	//loadModel();
+	loadPointCloud();
+	render();
+	animate();
 }
 
-function loadPointCloud(){
-  
+/*
+ * Load and display 3D point cloud
+ */
+function loadPointCloud() {
+	console.log('adding point cloud');
+	// grab data from the INPUT elements
+	var x = xarr ;
+	var y = yarr ;
+	var z = zarr ;
+	console.log(zarr);
+	console.log('Z LENGTHL: '+z.length);
+	// loop through the points and add them to the scene
+	for( var i=0 ; i < z.length ; i++ ) {
+		console.log(i);
+		// geometry describes the shape
+		geometry = new THREE.SphereGeometry( 10000, 16, 16 ) ;
+
+		// material describes the surface of the shape
+		var material = new THREE.MeshPhongMaterial( { color: 0xffffff } );
+
+		// mesh maps the material onto the geometry to make an object  
+		var mesh = new THREE.Mesh( geometry, material ) ;
+
+		// position the mesh in space
+		mesh.position.set( x[i], y[i], z[i] ) ;
+
+		mesh.material.side = THREE.DoubleSide; 
+
+		// add the mesh to the scene
+		scene.add( mesh ) ;
+	}
 }
 
 function loadModel(){
-    /* GEOMETRY */
-  var loader = new THREE.STLLoader();
-  loader.load(currentfile, function ( geometry ) {
-    var material = new THREE.MeshPhongMaterial( { color: 0xffffff } );
-    var mesh = new THREE.Mesh( geometry, material );
-    mesh.material.side = THREE.DoubleSide;
-    scene.add( mesh );
-  });
+	/* GEOMETRY */
+	var loader = new THREE.STLLoader();
+	loader.load(currentfile, function ( geometry ) {
+			var material = new THREE.MeshPhongMaterial( { color: 0xffffff } );
+			var mesh = new THREE.Mesh( geometry, material );
+			mesh.material.side = THREE.DoubleSide;
+			scene.add( mesh );
+			});
 }
 
 /*
@@ -110,34 +121,37 @@ function loadModel(){
  */
 function init() {
 
-  /* SCENE */
+	/* SCENE */
 
-  scene = new THREE.Scene(); // Create the scene 
-  scene.background = new THREE.Color( 0x222222 );
- 
-  /* RENDERER */
+	scene = new THREE.Scene(); // Create the scene 
+	scene.background = new THREE.Color( 0x222222 );
+
+	/* RENDERER */
 
 	renderer = new THREE.WebGLRenderer(); // Create the renderer
 	renderer.setPixelRatio( window.devicePixelRatio ); // tch
-
-  /* DOM CONTAINER FOR RENDERER */
+	/* DOM CONTAINER FOR RENDERER */
 
 	var container = document.getElementById( modelcontainerid ); //Assign the output to container
-  renderer.setSize($(container).width(), $(container).height());
+	renderer.setSize($(container).width(), $(container).height());
 	container.appendChild( renderer.domElement );
 
-  /* CAMERA */
+	/* CAMERA */
+	var VIEW_ANGLE = 75, 
+	    ASPECT = $(container).width()/$(container).height(), 
+	    NEAR = 0.1, FAR = 1000000;
+	camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR );
 
-	camera = new THREE.PerspectiveCamera( 60,$(container).width()/$(container).height(), 1, 1000 );
-	camera.position.z = 500;
+	//camera = new THREE.PerspectiveCamera( 60,$(container).width()/$(container).height(), 1, 1000 );
+	camera.position.z = cam_z;
 	camera.updateProjectionMatrix();
 	controls = new THREE.TrackballControls( camera,container);
-  //controls = new THREE.OrbitControls(camera);  
-  controls.staticMoving = false;	
+	//controls = new THREE.OrbitControls(camera);  
+	controls.staticMoving = false;	
 	controls.enableDamping = false;
 	controls.dampingFactor = 0.01;
 	controls.enableZoom = true;
-  controls.addEventListener( 'change', render ); // remove when using animation loop
+	controls.addEventListener( 'change', render ); // remove when using animation loop
 	// enable animation loop when using damping or autorotation
 
 
@@ -145,14 +159,14 @@ function init() {
 
 	var light = new THREE.DirectionalLight( 0xffffff );
 	light.position.set( 0, 0, 100 );
-  light.intensity=0.86;
+	light.intensity=0.86;
 	scene.add( light );
 	var light = new THREE.DirectionalLight( 0x002288 );
 	light.position.set( 0, 0, -1 );
 	scene.add( light );
 	var light = new THREE.DirectionalLight( 0x99cfff);
-  light.position.set(0,0,100);
-  light.intensity=0.2;
+	light.position.set(0,0,100);
+	light.intensity=0.2;
 	scene.add( light );
 	//
 	stats = new Stats();
@@ -165,7 +179,7 @@ function init() {
  * Event handler for window resizing
  */
 function onWindowResize() {
-  var container = document.getElementById( modelcontainerid ); //Assign the output to container
+	var container = document.getElementById( modelcontainerid ); //Assign the output to container
 	camera.aspect = $(container).width()/$(container).height();
 	camera.updateProjectionMatrix();
 	renderer.setSize( $(container).width(), $(container).height());
@@ -175,7 +189,7 @@ function onWindowResize() {
  * Function for animating the scene
  */
 function animate() {
-  requestAnimationFrame( animate );
+	requestAnimationFrame( animate );
 	controls.update(); // required if controls.enableDamping = true, or if controls.autoRotate = true
 	stats.update();
 	render();
@@ -185,5 +199,6 @@ function animate() {
  * Render the scene with a specified camera
  */
 function render() {
-  renderer.render( scene, camera );
+	renderer.render( scene, camera );
 }
+
