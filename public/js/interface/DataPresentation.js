658520fa30df63860e371729d9a2088fd3a3c893
Andrew Way <arw405@mun.ca>
2017-08-01 18:29:23 -0230

    Mesh working. Need to fix rotations of models

---
+++ public/js/interface/DataPresentation.js
@@ -0,0 +1,52 @@
+/*
+ * @file Data Presentation
+ */
+ 
+ /*
+ * Add toggle for displaying 3D Objects
+ */
+function addToggle(id,callback,name){
+	var element = document.createElement("input");
+	element.type="button";
+	element.name=name;
+	element.value=name;
+	element.onclick=callback;
+	element.setAttribute('onclick',  callback);
+	var toggleContainer=document.getElementById('pics');
+	toggleContainer.append(element);
+}
+
+/*
+ * Create the numerical displays and graphs
+ */
+function createAllDisplays(){
+	for(var i=0;i<dsstrings.length;i++){
+		var label=dsstrings[i];
+		if($("#graphs > div").length < autographlimit){
+			//create a graph
+			createDisplay('graph',label);
+		}
+		else{
+			//Create a monitor
+			createDisplay('monitor',label);
+		}
+	}
+}
+
+/*
+ * Add a new display
+ * param {String} type
+ * param {String} label
+ */
+function createDisplay(type,label){
+	if( type === 'graph'){
+		addChart(label);
+	}
+	else if(type === 'monitor'){
+		addDisplay(label);
+	}
+}
+function UpperCaseFirst(string){
+  return string.charAt(0).toUpperCase()+string.slice(1);
+}
+
--- public/js/interface/Mesh.js
+++ public/js/interface/Mesh.js
@@ -1,34 +1,36 @@
 
 
 var Mesh = function(sourceFile){
-    var M = new Model();
-    M.file = sourceFile;
+  var M = new Model();
+  M.file = sourceFile;
 
-    /**
-     * Load and add the mesh to the scene
-     */
-    M.loadModel = function(){
+  /**
+   * Load and add the mesh to the scene
+   */
+  M.loadModel = function(){
     var loader = new THREE.STLLoader();
+
     loader.load(M.file, function ( geometry ) {
-      var material = new THREE.MeshPhongMaterial( this.appearance.color );
-      this.mesh = new THREE.Mesh( geometry, material );
-      this.mesh.material.side = THREE.DoubleSide;
-      var Axis = new THREE.Vector3(1,0,0);
-      rotationAngle=-90*Math.PI/180;//Rotate by 90 degree
-      this.World.add( this.mesh );
-      rotateAroundWorldAxis(modelMesh, Axis,rotationAngle);
-      });
-    }
+        var material = new THREE.MeshPhongMaterial( this.appearance );
+        this.mesh = new THREE.Mesh( geometry, material );
+        this.mesh.material.side = THREE.DoubleSide;
+        var Axis = new THREE.Vector3(1,0,0);
+        M.rotationAngle=-90*Math.PI/180;//Rotate by 90 degree
+        M.World.add( this.mesh );
+        M.rotateAroundWorldAxis(this.mesh, Axis,rotationAngle);
+        });
+  }
 
-    /**
-     * Set the current 3D model filepath on the public directory
-     * @param {String} file
-     */
-    M.setfile = function(file){
-      this.file=file;
-    };
+  /**
+   * Set the current 3D model filepath on the public directory
+   * @param {String} file
+   */
+  M.setfile = function(file){
+    this.file=file;
+  };
 
-    return Mesh;
-})();
+  return M;
+};
+
 
 
